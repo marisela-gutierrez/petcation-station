3b8cb72f8e215618b6705ef654d4149a7ec11a5e
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-28 13:13:07 -0500

    added review routes and model. Connected reviews to pet sitter and pet owner

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const petOwnerRoutes = require('./petOwner-routes');
 const petSitteroutes = require('./petSitter-routes');
+const reviewRoutes = require('./review-routes')
 
 router.use('/users', userRoutes);
 router.use('/petOwners', petOwnerRoutes);
 router.use('/petSitters', petSitteroutes)
+router.use('/reviews', reviewRoutes)
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Pet_Owner = require('./Pet_Owner');
 const Pet_Sitter = require('./Pet_Sitter');
+const Review = require('./Review');
 
 User.hasOne(Pet_Owner, {
     foreignKey: 'user_id'
@@ -18,4 +19,32 @@ Pet_Sitter.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Pet_Owner, Pet_Sitter };
+Pet_Owner.belongsToMany(Pet_Sitter, {
+    through: Review,
+    as: 'reviewed_sitter',
+    foreignKey: 'pet_owner_id'
+});
+
+Pet_Sitter.belongsToMany(Pet_Owner, {
+    through: Review,
+    as: 'reviewed_sitter',
+    foreignKey: 'pet_sitter_id'
+})
+
+Review.belongsTo(Pet_Owner, {
+    foreignKey: 'pet_owner_id'
+});
+
+Review.belongsTo(Pet_Sitter, {
+    foreignKey: 'pet_sitter_id'
+});
+
+Pet_Owner.hasMany(Review, {
+    foreignKey: 'pet_owner_id'
+});
+
+Pet_Sitter.hasMany(Review, {
+    foreignKey: 'pet_sitter_id'
+});
+
+module.exports = { User, Pet_Owner, Pet_Sitter, Review };
