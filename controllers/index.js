e7f5e13529cc9e63d3b2307fa0b157ca0f9eb021
Virginia Rabello <virginia.rabello2107@gmail.com>
2021-06-29 14:12:49 -0500

    dashboard handlebar html

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
