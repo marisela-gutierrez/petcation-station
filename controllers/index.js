2e2af905be8a953313cb58f49dc42d7c83309aa6
marigut <mari.gutierrez96@yahoo.com>
2021-06-29 19:25:06 -0500

    pet profiles routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const petMiniRoutes = require('./petMini-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/petProfiles', petMiniRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
