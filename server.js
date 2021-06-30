ebeb0915056db36f371f9c6b52032df612db2c94
Virginia Rabello <virginia.rabello2107@gmail.com>
2021-06-30 11:09:11 -0500

    Solving merges issues

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ const sess = {
 };
 
 const hbs = exphbs.create({ helpers });
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./controllers/'));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(session(sess));
 
