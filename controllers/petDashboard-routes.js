c90b99bdfedc5b9f40d1552a4b5a3bb938ebedc1
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-30 12:42:22 -0500

    began working on petDash routes

---
+++ controllers/petDashboard-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
